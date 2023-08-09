def4961a982981e46be38976d57e45df5453118c
hrano1000 <126633269+hrano1000@users.noreply.github.com>
2023-08-09 22:47:52 +0100

    Update dashboard.tsx

--- pages/exchange/dashboard.tsx
+++ pages/exchange/dashboard.tsx
@@ -129,7 +129,7 @@ const Dashboard: NextPage = () => {
   );
    useEffect(() => {
     const script = document.createElement("script");
-    script.src = "//code.tidio.co/shvspmauu8c7duhzd4kali3m9bvgorsd.js";
+    script.src = "//code.tidio.co/bedbtgoe0oiwizosuo1orfnehjmkkoz3.js";
     script.async = true;
     document.head.appendChild(script);
   }, []);
